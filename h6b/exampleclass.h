21cc4de125b3f43c84a278b44585ab173bfc1f85
KassiKassinen <aleksisakari.makinen@gmail.com>
2025-12-14 19:46:45 +0200

    Viikkotehtävä 6b

---
+++ h6b/exampleclass.h
@@ -0,0 +1,20 @@
+#ifndef EXAMPLECLASS_H
+#define EXAMPLECLASS_H
+#include <QThread>
+#include <QObject>
+#include <QCoreApplication>
+
+class ExampleClass : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ExampleClass(QObject *parent = nullptr);
+    void startToWait();
+public slots:
+    void sayHelloSlot();
+
+signals:
+    void readyToSay();
+};
+
+#endif // EXAMPLECLASS_H
